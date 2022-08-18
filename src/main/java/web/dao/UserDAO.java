c9f7978323b940ba495e2a1530efed94d02e4ac8
Ratean <es_hay_em@list.ru>
2022-08-18 21:21:37 +0400

    2.3.1_настройка AppConfig, добавление интерфейсов DAO, Service

---
+++ src/main/java/web/dao/UserDAO.java
@@ -0,0 +1,13 @@
+package web.dao;
+
+import web.model.User;
+
+import java.util.List;
+
+public interface UserDAO {
+
+    void save(User user);
+    void update(User user);
+    void delete(User user);
+    List<User> getAllUser();
+}
